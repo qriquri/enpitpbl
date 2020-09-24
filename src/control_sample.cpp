ae83aaee1ca93a92c5e0aabcdb1e204b6dc7e80b
moko <57133526+qriquri@users.noreply.github.com>
2020-09-24 01:22:53 +0000

    add control system

--- src/control_sample.cpp
+++ src/control_sample.cpp
@@ -9,9 +9,14 @@
 // %EndTag(MSG_HEADER)%
 
 #include <sstream>
+#include <string.h>
+#include <stdlib.h>
+#include<cmath>
 //custommessage
 #include <gnc/command.h>
 #include <gnc/GoalPos.h>
+
+#include <SDL2/SDL.h>
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
@@ -52,44 +57,104 @@ int main(int argc, char **argv)
 // %Tag(FILL_MESSAGE)%
     //std_msgs::String command;
     gnc::command command;
-    gnc::GoalPos position;
+    gnc::GoalPos position; 
     //std::stringstream ss;
-    //SELECT COMMAND
-    int msg;
-    float x = 0; float y = 0; float z = 0;
-    //printf("%d %d %d", 'w', "w", 'a');
-    if(stop_flag == 1){
-      printf("1:UP\n2:DOWN\n3:FRONT\n4:BACK\n5:LEFT\n6:RITHI\n7:STOP\n0:HALT\n");
-      printf("And set XYZ when you use 1~6 command.\n");
-    }else{
-      printf("Please stop before next action!!!!\n7:STOP\n");
+    char str[10];	
+    char cmd[2];		
+    char chDist[10];
+    double dDist = 0;		
+
+    printf("Chose command.\nq w e * * * u i * *\n a s d * * h * *l\n z x c * * * *\n");
+    printf("And Setting distance!!\n");
+    scanf("%s", str);
+
+    strncpy(cmd, str, 1);
+    cmd[1] = '\0';
+
+    strncpy(chDist, str + 1, strlen(str) - 1);
+    chDist[strlen(str) - 1] = '\0';
+    dDist = atof(chDist);
+
+	
+    if(!dDist){
+      dDist = 0.5;
     }
-    scanf("%d %f %f %f", &msg, &x, &y, &z);
-    command.x = x;
-    command.y = y;
-    command.z = z;
-    switch(msg){
-       case 4:
-        command.com = "back"; stop_flag =0; break;
-       case 2:
-        command.com = "down"; stop_flag =1; break;
-       case 3: 
-        command.com = "front";stop_flag =0; break;
-       case 0:
-        command.com = "halt"; stop_flag =1; break;
-       case 5:
-        command.com = "left"; stop_flag =0; break;
-       case 6:
-        command.com = "right"; stop_flag =0; break;
-       case 7:
-        command.com = "stop"; stop_flag =1; break;
-       
-       case 1:
-        command.com = "up"; stop_flag =1; break;
-       default:
-        break;
-    }
-    //ss << tmp << count;
+    
+    switch(cmd[0]){
+        case 'u':
+          command.com = "up"; 
+          command.z = dDist;
+          
+          break;
+        case 'i':
+          command.com = "down"; 
+          command.z = -dDist;
+          
+          break;
+        case 'w':
+          command.com = "front"; 
+          command.y = dDist;
+          
+          break;
+        case 'e':
+          command.com = "rfront"; 
+          command.x = dDist/sqrt(2);
+          command.y = dDist/sqrt(2);
+          //stop_flag =1; 
+          break;
+        case 'd': 
+          command.com = "right";
+          command.x = dDist;
+          //stop_flag =0; 
+          break;
+        case 'c':
+          command.com = "rback"; 
+          command.x = dDist/sqrt(2);
+          command.y = -dDist/sqrt(2);
+          //stop_flag =1; 
+          break;
+        case 'x':
+          command.com = "back";
+          command.y = -dDist;
+          // stop_flag =0; 
+          break;
+        case 'z':
+          command.com = "lback";
+          command.x = -dDist/sqrt(2);
+          command.y = -dDist/sqrt(2);
+          // stop_flag =0; 
+          break;
+        case 'a':
+          command.com = "left"; 
+          command.x = -dDist;
+          break;
+        case 'q':
+          command.com = "lfront";
+          
+          command.x = -dDist/sqrt(2);
+          command.y = dDist/sqrt(2);
+          // stop_flag =1; 
+          break;
+        case 's':
+          command.com = "stop"; 
+          stop_flag =1; 
+          break;
+        case 'm':
+          command.com = "mark"; 
+          
+          break;
+        case 'l':
+          command.com = "land"; 
+          break;
+        case 'h':
+          command.com = "home";
+          command.x = 0;
+          command.y = 0; 
+          break;
+        default:
+          break;
+      }
+      
     
 // %EndTag(FILL_MESSAGE)%
 
@@ -98,10 +163,10 @@ int main(int argc, char **argv)
     ROS_INFO("%f %f %f", command.x, command.y, command.z);
    char com[10];
 
-sprintf(com, "%s", command.com.c_str());
-   if(strcmp("halt", com) == 0){
-     printf("sucsess\n");
-}
+    sprintf(com, "%s", command.com.c_str());
+    if(strcmp("halt", com) == 0){
+        printf("sucsess\n");
+    }
 // %EndTag(ROSCONSOLE)%
 
    
